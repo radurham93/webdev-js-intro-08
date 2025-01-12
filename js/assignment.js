64b2121ba0b98984e6a2690ea0f29e0fe80e1ada
Robert Durham <radurham93@gmail.com>
2025-01-12 17:59:47 -0500

    completed assignment

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,71 @@ const largestNumberElement = document.getElementById("largest-number");
 const averageNumberElement = document.getElementById("average-number");
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+
+
+function findSmallestNumber(array){
+
+let smallest = array[0];
+for (let number of myNumbers) {
+    if (number < smallest) {
+        smallest = number
+    }
+}
+return smallest;
+
+}
+
+function findLargestNumber(array){
+
+        let largest = array[0];
+        for (let number of myNumbers) {
+            if (number > largest) {
+             largest = number
+            }
+        }
+        return largest;
+}
+
+function findAverage(array){
+    let sum = 0;
+    for (let number of myNumbers) {
+        sum += number;
+    }
+    return sum / array.length;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function render() {
+    // Call the created functions
+smallestNumberElement.textContent = findSmallestNumber(myNumbers);
+largestNumberElement.textContent = findLargestNumber(myNumbers);
+averageNumberElement.textContent = findAverage(myNumbers);
+}
+
+submissionBtn.addEventListener("click", function () {
+    render();
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+let numbers = [1, 2, 3, 4, 5];
+ /*
+for (let i = 0; i < numbers.length; i++) {
+console.log(numbers[i]);
+}
+
+let sum = 0;
+
+for (let number of numbers){
+    sum += number;
+}
+console.log(sum);
+
+let digits = [1, 2, 3, 4, 5];
+for (let i = 0; i < digits.length; i++) {
+    digits[i] += 10;
+}
+console.log(digits);
+
+
+numbers.forEach(function(numbers) {
+    console.log(numbers);
+});
+
+for (let num of numbers) {
+    num += 4;
+    console.log(num);
+}
+console.log(numbers);
+
+
+numbers.forEach(function(number, index, array) {
+array[index] = number + 4;
+});
+console.log(numbers);
+*/
